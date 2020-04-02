029ed68dc0b4932bf11c389ae147b8ed386bbd8c
xuwwenke <3492517331@qq.com>
2020-04-02 20:03:07 +0800

    note

---
+++ editor/app.js
@@ -0,0 +1,22 @@
+var np = {};                // Notepad 主程序对象
+np.config = {
+  'appContainer': '.notepad-app'
+};
+np.bShowStatusBar = false;   // 是否显示状态栏
+np.bLineWrap = false;        // 是否换行
+/* global $menubar $editor $statusbar: true */
+
+/*
+
+$(function() {
+
+  $menubar.show();
+
+  $editor.show();
+
+  //$statusbar.init({'row': 1, 'col': 1});
+
+  $statusbar.show();
+
+});
+*/
